afe88f9235b98ad87a15a48a3000327303073c35
teena0010 <teenasharmaa001@gmail.com>
2026-07-10 19:36:40 +0530

    Update celery.py

--- bookmyseat/celery.py
+++ bookmyseat/celery.py
@@ -6,3 +6,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
 app = Celery('bookmyseat')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+app.conf.broker_transport_options = {
+    'ssl': {
+        'cert_reqs': 'CERT_REQUIRED',
+    },
+    'socket_connect_timeout': 30,
+}
